306213f932932505c754cd59ed1316e6d29e188b
Lakshay <lakshay.sbp@gmail.com>
2023-11-06 19:49:43 +0530

    settiing up the home pg

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,21 @@
+"use client";
+import { Card, Divider, Subtitle, Text } from "@tremor/react";
 
 export default function Home() {
   return (
-    <main className="text-6xl">
-      <h1>
-  Hello
-      </h1>
-    </main>
-  )
+    <div className="min-h-screen flex flex-col justify-center items-center max-w-3xl">
+      <Card>
+      <Text className="text-xl font-bold text-center mb-10">Weather AI</Text>
+      <Subtitle className="text-xl text-center">
+        Powered by Lakshay
+      </Subtitle>
+      <Divider className="my-10"/>
+      
+      <Card className=""> 
+        {/* <CityPicker/> */}
+      </Card>
+      
+      </Card>
+    </div>
+  );
 }
